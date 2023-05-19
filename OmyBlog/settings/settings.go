ecf9f28eabb6813598501444d57240834883dfcb
siming <524131399@qq.com>
2023-05-19 14:54:35 +0800

    创建user表

--- OmyBlog/settings/settings.go
+++ OmyBlog/settings/settings.go
@@ -47,9 +47,10 @@ type RedisConfig struct {
 }
 
 func Init() (err error) {
-	viper.SetConfigName("config") // 配置文件名称(无扩展名)
-	viper.SetConfigType("yaml")   // 如果配置文件的名称中没有扩展名，则需要配置此项
-	viper.AddConfigPath(".")      // 查找配置文件所在的路径
+	viper.SetConfigFile("config.yaml")
+	// viper.SetConfigName("config") // 配置文件名称(无扩展名)
+	// viper.SetConfigType("yaml")   // 如果配置文件的名称中没有扩展名，则需要配置此项
+	viper.AddConfigPath(".") // 查找配置文件所在的路径
 
 	err = viper.ReadInConfig() // 查找并读取配置文件
 	if err != nil {            // 处理读取配置文件的错误
@@ -65,6 +66,9 @@ func Init() (err error) {
 	viper.WatchConfig()
 	viper.OnConfigChange(func(in fsnotify.Event) {
 		fmt.Println("配置文件修改了!")
+		if err := viper.Unmarshal(Conf); err != nil {
+			fmt.Printf("viper.Unmarshal failed, err:%v\n", err)
+		}
 	})
 	return
 }
